ccc8690321410b7ccd0bae7f1c4212c59aa6659b
rzdor <ruslan_zdor@epam.com>
2021-12-29 11:19:53 -0600

    Add project files.

---
+++ AzureFunction/Test.cs
@@ -0,0 +1,33 @@
+﻿#define CONDITION1
+#define CONDITION2
+using System;
+using System.Diagnostics;
+
+namespace AzureFunction
+{
+    class Test
+    {
+        static void Main()
+        {
+            Console.WriteLine("Calling Method1");
+            Method1(3);
+            Console.WriteLine("Calling Method2");
+            Method2();
+
+            Console.WriteLine("Using the Debug class");
+            Debug.WriteLine("DEBUG is defined");
+        }
+
+        [Conditional("CONDITION1")]
+        public static void Method1(int x)
+        {
+            Console.WriteLine("CONDITION1 is defined");
+        }
+
+        [Conditional("CONDITION1"), Conditional("CONDITION2")]
+        public static void Method2()
+        {
+            Console.WriteLine("CONDITION1 or CONDITION2 is defined");
+        }
+    }
+}
