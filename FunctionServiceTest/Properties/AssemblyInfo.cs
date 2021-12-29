97185104411e86e34c5b5a37e17a0d763e687758
rzdor <ruslan_zdor@epam.com>
2021-12-29 11:30:20 -0600

    add unit test project

---
+++ FunctionServiceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FunctionServiceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("FunctionServiceTest")]
+[assembly: AssemblyCopyright("Copyright © HP 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("efa1584c-8801-4e55-b916-7adf90fcfe68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
